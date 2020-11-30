f69f77bc3847f169a2c73421bc33f93a14184834
ans97j <abilio.navarro@getronics.com>
2020-11-30 15:07:42 +0100

    Modificaciones para generar jars y war del proyecto

--- eeutil-vis-docexp/src/main/java/es/mpt/dsic/inside/ws/service/model/eni/documento/TipoContenido.java
+++ eeutil-vis-docexp/src/main/java/es/mpt/dsic/inside/ws/service/model/eni/documento/TipoContenido.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (C) 2012-13 MINHAP, Gobierno de España This program is licensed and may be used,
- * modified and redistributed under the terms of the European Public License (EUPL), either version
- * 1.1 or (at your option) any later version as soon as they are approved by the European
- * Commission. Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
- * more details. You should have received a copy of the EUPL1.1 license along with this program; if
- * not, you may find it at http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- */
-
 
 package es.mpt.dsic.inside.ws.service.model.eni.documento;
 
@@ -19,11 +8,9 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>
- * Java class for TipoContenido complex type.
+ * <p>Java class for TipoContenido complex type.
  * 
- * <p>
- * The following schema fragment specifies the expected content contained within this class.
+ * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
  * &lt;complexType name="TipoContenido">
@@ -43,51 +30,60 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "TipoContenido",
-    namespace = "https://ssweb.seap.minhap.es/Eeutil/XSD/v1.0/documento/contenido",
-    propOrder = {"valorBinario", "referencia"})
+@XmlType(name = "TipoContenido", namespace = "https://ssweb.seap.minhap.es/Eeutil/XSD/v1.0/documento/contenido", propOrder = {
+    "valorBinario",
+    "referencia"
+})
 public class TipoContenido {
 
-  @XmlElement(name = "ValorBinario")
-  protected byte[] valorBinario;
-  protected String referencia;
+    @XmlElement(name = "ValorBinario")
+    protected byte[] valorBinario;
+    protected String referencia;
 
-  /**
-   * Gets the value of the valorBinario property.
-   * 
-   * @return possible object is byte[]
-   */
-  public byte[] getValorBinario() {
-    return valorBinario;
-  }
+    /**
+     * Gets the value of the valorBinario property.
+     * 
+     * @return
+     *     possible object is
+     *     byte[]
+     */
+    public byte[] getValorBinario() {
+        return valorBinario;
+    }
 
-  /**
-   * Sets the value of the valorBinario property.
-   * 
-   * @param value allowed object is byte[]
-   */
-  public void setValorBinario(byte[] value) {
-    this.valorBinario = ((byte[]) value);
-  }
+    /**
+     * Sets the value of the valorBinario property.
+     * 
+     * @param value
+     *     allowed object is
+     *     byte[]
+     */
+    public void setValorBinario(byte[] value) {
+        this.valorBinario = ((byte[]) value);
+    }
 
-  /**
-   * Gets the value of the referencia property.
-   * 
-   * @return possible object is {@link String }
-   * 
-   */
-  public String getReferencia() {
-    return referencia;
-  }
+    /**
+     * Gets the value of the referencia property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReferencia() {
+        return referencia;
+    }
 
-  /**
-   * Sets the value of the referencia property.
-   * 
-   * @param value allowed object is {@link String }
-   * 
-   */
-  public void setReferencia(String value) {
-    this.referencia = value;
-  }
+    /**
+     * Sets the value of the referencia property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReferencia(String value) {
+        this.referencia = value;
+    }
 
 }
